ed6a8a21f8b29ea62be5d72b74d18cc4b2112c79
Willians Santos <taldowill29@gmail.com>
2023-09-19 22:08:30 -0400

    adicionando fonte do header

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,19 @@
 export default {
 	content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
 	theme: {
-		extend: {},
+		extend: {
+			fontFamily: {
+				Vigilance: "'Vigilance BRK', sans-serif;",
+			},
+			colors: {
+				Orange: {
+					400: "#FF9A62",
+					500: "#FF7C33",
+				},
+				Black: "#0D0D0D",
+				White: "#F2F2F2",
+			},
+		},
 	},
 	plugins: [],
 };
