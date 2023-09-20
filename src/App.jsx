d95df29192d6f1d7e65279fd7edda9d0d7b6f613
Willians Santos <taldowill29@gmail.com>
2023-09-19 22:25:03 -0400

    criando rota de novo post

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
+import { Outlet } from "react-router-dom";
+import Header from "./components/Header";
+
 function App() {
 	return (
 		<>
-			<div>TechBlog</div>
+			<Header />
+			<Outlet />
 		</>
 	);
 }
--- src/index.jsx
+++ src/index.jsx
@@ -2,9 +2,19 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
+import NewPost from "./pages/NewPost.jsx";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+const routes = createBrowserRouter([
+	{
+		element: <App />,
+		path: "/",
+		children: [{ element: <NewPost />, path: "/nova-postagem" }],
+	},
+]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
 	<React.StrictMode>
-		<App />
+		<RouterProvider router={routes} />
 	</React.StrictMode>
 );
